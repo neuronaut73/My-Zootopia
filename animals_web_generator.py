d2ed511ce7cd11c410b0e485417167a1116e0585
Neuronaut <patrec.ug@gmail.com>
2025-04-26 17:17:40 +0200

    deleted my original print section

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -9,24 +9,6 @@ def load_data(file_path):
 
 animals_data = load_data('animals_data.json')
 
-# print(animals_data)
-
-for dict in animals_data:
-    name = dict["name"]
-    location = dict["locations"][0]
-    diet = dict["characteristics"]["diet"]
-    type_fox = dict["characteristics"].get("type")
-
-    variables = [name, location, diet, type_fox]
-
-    if all(var is not None for var in variables):
-       print(f"""
-Name: {name}
-Diet: {diet}
-Location: {location}
-Type: {type_fox}
-             """)
-
 
 output = ''  # define an empty string
 for dict in animals_data:
