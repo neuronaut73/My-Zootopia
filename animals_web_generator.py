f2530768db362b23fbdfd3061b068384b847b216
Neuronaut <patrec.ug@gmail.com>
2025-04-26 17:16:59 +0200

    added the generation of an output string

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -27,3 +27,24 @@ Location: {location}
 Type: {type_fox}
              """)
 
+
+output = ''  # define an empty string
+for dict in animals_data:
+    name = dict["name"]
+    location = dict["locations"][0]
+    diet = dict["characteristics"]["diet"]
+    type_fox = dict["characteristics"].get("type")
+
+    variables = [name, location, diet, type_fox]
+
+    if all(var is not None for var in variables):
+        # append information to each string
+        output += f"Name: {name}\n"
+        output += f"Diet: {diet}\n"
+        output += f"Location: {location}\n"
+        output += f"Type: {type_fox}\n"
+
+print(output)
+
+
+
